8c4df182ee7fa7a85f8b9a622c357171e4ef6f85
cyril <pichard.cyril@laposte.net>
2012-10-28 16:54:24 +0000

    wip

--- HOpenFX.hs
+++ HOpenFX.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE ForeignFunctionInterface, DeriveDataTypeable  #-}
-module HOpenFX where
 -- OpenFX API Hacking in haskell
 --
--- Main interest is to test the C foreign function interface in haskell
+-- Main interest is testing the C foreign function interface in haskell
 --
--- docs from : 
+-- docs from :
 -- http://weblog.haskell.cz/pivnik/building-a-shared-library-in-haskell/
 --
--- These two functions have to be implemented to initiate the communication 
+-- These two functions have to be implemented to initiate the communication
 -- with the host :
---    int OfxGetNumberOfPlugins(void) 
+--    int OfxGetNumberOfPlugins(void)
 --      - identifies how many plug-ins are implemented in the file,
---    OfxPlugin *OfxGetPlugin(int N) 
+--    OfxPlugin *OfxGetPlugin(int N)
 --      - returns a structure identifying the Nth plug-in in the file.
 
 --
 -- The OfxPlugin has a predefined structure
 --
 --
+module HOpenFX where
+
 import Foreign
 import Foreign.ForeignPtr
 import OfxCore
@@ -27,9 +28,9 @@ import Control.Applicative
 import Data.IORef
 import OfxProperty
 
--- |Global state variable to store the pointer to the host given by the 
+-- |Global state variable to store the pointer to the host given by the
 -- host itself
-gHost :: IORef HOfxHostPtr 
+gHost :: IORef HOfxHostPtr
 gHost = unsafePerformIO (newIORef nullPtr)
 {-# NOINLINE gHost #-}
 
@@ -43,7 +44,7 @@ setOfxHost :: HOfxHostPtr -> IO ()
 setOfxHost h = do
     print h
     putStrLn "haskell:setOfxHost passed"
-    writeIORef gHost h    
+    writeIORef gHost h
     putStrLn "haskell:host stored in IORef"
     return ()
 
@@ -52,12 +53,12 @@ setOfxHost h = do
 foreign export ccall "mainEntryFunc" mainEntryFunc :: (CString -> Ptr () -> Ptr () -> Ptr () -> IO CInt)
 foreign import ccall "&mainEntryFunc" mainEntryFuncPtr :: FunPtr (CString -> Ptr () -> Ptr () -> Ptr () -> IO CInt)
 mainEntryFunc :: CString -> Ptr () -> Ptr () -> Ptr () -> IO CInt
-mainEntryFunc a _ _ _   = do 
+mainEntryFunc a _ _ _   = do
     putStrLn "haskell:mainEntryFunc called"
     action <- peekCString a
     print action
     -- TODO : return kOfxStatOk
-    -- find a way to pass all the define constants 
+    -- find a way to pass all the define constants
     host <- readIORef gHost
     print host
     return 0
